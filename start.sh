e1cb4f660c065ab7df02f65bf7740d6de3adeb22
yossefhs <33151711+yossefhs@users.noreply.github.com>
2026-04-14 17:16:04 +0300

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Ajouter le dossier bin local de Python au PATH
-export PATH=$PATH:$HOME/.local/bin:$(python3 -m site --user-base)/bin
-
-# S'assurer d'installer le paquet Python notebooklm-py
-pip install --user notebooklm-py || pip3 install --user notebooklm-py
-
-# Lancer le serveur Express
 node index.js
